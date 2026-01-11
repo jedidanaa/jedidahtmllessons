90e43ac1c068c9e04486a0f031decad2f3846699
jedidanaa <jedidanaashidaaoduahnortey@gmail.com>
2026-01-11 18:41:13 +0000

    read mode

--- Pythonlessons/filehandling/read_mode.py
+++ Pythonlessons/filehandling/read_mode.py
@@ -1,6 +1,11 @@
-file = open("example.txt","r")
+file = open("reading_.txt","w")
+file.write("hello world\n It's fun learning with file handling methods\n")
 
-f = file.read()
-print(f)
+file.close()
 
+# the read method displays all content of the file
+f = open("reading_.txt", "r")
+print(f.read())
+# reading a specific num of characters put the num inside the read parentheis
+# print(f.read(15))
 file.close()
